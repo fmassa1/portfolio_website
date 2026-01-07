ff4213a41a6bbbcac579b82687e13698df92ab30
Fredric Massa <rickmass2020@gmail.com>
2026-01-07 11:55:20 -0600

    updated experience

--- src/pages/Experiences.tsx
+++ src/pages/Experiences.tsx
@@ -30,6 +30,35 @@ export default function Experiences() {
                     <li>Optimized 3D model generation pipelines, increasing AR throughput by <strong>40%</strong></li>
                 </ul>
             </div>
+
+            <div className="experience-card">
+                <h2>Argonne National Laboratory</h2>
+                <p><strong>Software Engineer Intern</strong> <span className="muted">(May 2024 – Aug 2024)</span></p>
+                <p>
+                    Developed an AI assistant  in Swift for the Apple Vision Pro that allows general querying, 3D model manipulation, and task automation.
+                </p>
+                <ul>
+                    <li>Utilized OpenAI and Llama APIs with Python to power domain-specific natural language interactions, supporting 
+                    dynamic content generation, calendar scheduling, and supercomputer task automation.</li>
+                    <li>Presented at Sandia National Lab XR Conference 2024 to an audience of <strong>200+</strong>.</li>
+                </ul>
+            </div>
+
+            <div className="experience-card">
+                <h2>Argonne National Laboratory</h2>
+                <p><strong>Software Engineer Intern</strong> <span className="muted">(May 2023 – Aug 2023)</span></p>
+                <p>
+                Analyzed the effectiveness of pedestrian safety by tracking vehicle speeds and sudden stops, leveraging TensorFlow 
+                for vehicle detection and PyTorch deep learning models for re-identification and speed classification.
+                </p>
+                <ul>
+                    <li>Designed a MariaDB database and decreased SQL query execution time by <strong>27%</strong> by optimizing queries.</li>
+                    <li>Worked on the data portal to view and analyze the data using Django, Python, HTML, CSS</li>
+                    <li>Developed analysis tools using Python to compute average vehicle speeds and identify traffic flow patterns 
+                    from large-scale transportation datasets.</li>
+                </ul>
+            </div>
+            
         </>
     );
 }
